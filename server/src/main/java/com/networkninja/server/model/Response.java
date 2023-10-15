13d0a59cbc275732f41680d8224c89afc8da0dbe
akhilajohnsonc <59962882+akhilajohnsonc@users.noreply.github.com>
2023-10-15 17:52:46 +0530

    Response model added

---
+++ server/src/main/java/com/networkninja/server/model/Response.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.networkninja.server.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author Akhila Johnson C
+ *
+ */
+@Data
+@SuperBuilder
+@JsonInclude(Include.NON_NULL)
+public class Response {
+	protected LocalDateTime timeStamp;
+	protected int statusCode;
+	protected HttpStatus status;
+	protected String reason;
+	protected String message;
+	protected String developerMessage;
+	protected Map<?,?> data;
+}
