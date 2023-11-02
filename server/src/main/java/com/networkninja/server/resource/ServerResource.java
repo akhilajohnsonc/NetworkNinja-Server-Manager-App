e1fc1cf19c265914fdef4fac49a5abc57fd34552
akhilajohnsonc <59962882+akhilajohnsonc@users.noreply.github.com>
2023-11-02 20:12:17 +0530

    sleep added for getServers function and formated file

--- server/src/main/java/com/networkninja/server/resource/ServerResource.java
+++ server/src/main/java/com/networkninja/server/resource/ServerResource.java
@@ -1,6 +1,7 @@
 package com.networkninja.server.resource;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
@@ -27,84 +28,59 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/server")
 @RequiredArgsConstructor
 public class ServerResource {
-    private final ServerServiceImpl serverService;
+	private final ServerServiceImpl serverService;
 
-    @GetMapping("/list")
-    public ResponseEntity<Response> getServers() {
-        LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
+	@GetMapping("/list")
+	public ResponseEntity<Response> getServers() throws InterruptedException {
+		LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		TimeUnit.SECONDS.sleep(3);
+		return ResponseEntity
+				.ok(Response.builder().timeStamp(currentDateTime).data(Map.of("servers", serverService.list(30)))
+						.message("Servers retrieved").status(HttpStatus.OK).statusCode(HttpStatus.OK.value()).build());
+	}
 
-        return ResponseEntity
-                .ok(Response.builder()
-                        .timeStamp(currentDateTime)
-                        .data(Map.of("servers", serverService.list(30)))
-                        .message("Servers retrieved")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build());
-    }
+	@GetMapping("/ping/{ipAddress}")
+	public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+		Server server = serverService.ping(ipAddress);
 
-    @GetMapping("/ping/{ipAddress}")
-    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
-        Server server = serverService.ping(ipAddress);
+		LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
 
-        LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		return ResponseEntity.ok(Response.builder().timeStamp(currentDateTime).data(Map.of("server", server))
+				.message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed").status(HttpStatus.OK)
+				.statusCode(HttpStatus.OK.value()).build());
+	}
 
-        return ResponseEntity
-                .ok(Response.builder()
-                        .timeStamp(currentDateTime)
-                        .data(Map.of("server", server))
-                        .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build());
-    }
+	@PostMapping("/save")
+	public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+		LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
 
-    @PostMapping("/save")
-    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
-        LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		return ResponseEntity.ok(Response.builder().timeStamp(currentDateTime)
+				.data(Map.of("server", serverService.create(server))).message("Server created")
+				.status(HttpStatus.CREATED).statusCode(HttpStatus.CREATED.value()).build());
+	}
 
-        return ResponseEntity
-                .ok(Response.builder()
-                        .timeStamp(currentDateTime)
-                        .data(Map.of("server", serverService.create(server)))
-                        .message("Server created")
-                        .status(HttpStatus.CREATED)
-                        .statusCode(HttpStatus.CREATED.value())
-                        .build());
-    }
+	@GetMapping("/get/{id}")
+	public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
 
-    @GetMapping("/get/{id}")
-    public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
+		LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
 
-        LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		return ResponseEntity
+				.ok(Response.builder().timeStamp(currentDateTime).data(Map.of("server", serverService.get(id)))
+						.message("Server retrieved").status(HttpStatus.OK).statusCode(HttpStatus.OK.value()).build());
+	}
 
-        return ResponseEntity
-                .ok(Response.builder()
-                        .timeStamp(currentDateTime)
-                        .data(Map.of("server", serverService.get(id)))
-                        .message("Server retrieved")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build());
-    }
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+		LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
 
-        LocalDateTime currentDateTime = Instant.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		return ResponseEntity
+				.ok(Response.builder().timeStamp(currentDateTime).data(Map.of("Deleted", serverService.delete(id)))
+						.message("Server deleted").status(HttpStatus.OK).statusCode(HttpStatus.OK.value()).build());
+	}
 
-        return ResponseEntity
-                .ok(Response.builder()
-                        .timeStamp(currentDateTime)
-                        .data(Map.of("Deleted", serverService.delete(id)))
-                        .message("Server deleted")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build());
-    }
-    
-    @GetMapping(value = "/image/{fileName}", produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
-		return Files.readAllBytes(Paths.get(System.getProperty("user.home")+ "/Downloads/images/" + fileName));
-    }
+	@GetMapping(value = "/image/{fileName}", produces = MediaType.IMAGE_PNG_VALUE)
+	public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+		return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Downloads/images/" + fileName));
+	}
 }
