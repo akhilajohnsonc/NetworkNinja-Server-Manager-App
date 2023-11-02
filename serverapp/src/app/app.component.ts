1efc3250912e318cb118f96d579bbee33d4a4915
akhilajohnsonc <59962882+akhilajohnsonc@users.noreply.github.com>
2023-11-02 20:13:10 +0530

    Added enum, server services and index page

--- serverapp/src/app/app.component.ts
+++ serverapp/src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { DataState } from './enum/data-state.enum';
+import { Status } from './enum/status.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { Server } from './interface/server';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'serverapp';
+  title = 'Network Ninja';
+  appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  filterStatus$ = this.filterSubject.asObservable();
+  constructor(
+    private ServerService: ServerService
+  ) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.ServerService.servers$
+    .pipe(
+      map((response) => {
+        this.dataSubject.next(response);
+
+        return {
+          dataState: DataState.LOADED_STATE,appData: response}
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.ServerService.ping$(ipAddress)
+      .pipe(
+        map(response => {
+          const index = this.dataSubject.value.data.servers.findIndex(server =>  server.id === response.data.server.id);
+          this.dataSubject.value.data.servers[index] = response.data.server;
+          // this.notifier.onDefault(response.message);
+          this.filterSubject.next('');
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.filterSubject.next('');
+          // this.notifier.onError(error);
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
 }
